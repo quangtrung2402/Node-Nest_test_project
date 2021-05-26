ac56ab4168290505b57d1667b174800202cb096a
Ngo Quang Trung <quangtrung2402@gmail.com>
2021-05-26 23:49:11 +0700

    Add request, redirect, param, sub-domain

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,10 +1,11 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Req, Redirect, Param } from '@nestjs/common';
 import { AppService } from './app.service';
+import { Request } from 'express';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
-
+  
   @Get()
   getHello(): string {
     return this.appService.getHello();
@@ -14,11 +15,31 @@ export class AppController {
 @Controller('cats')
 export class CatsController {
   @Get()
-  find(): string {
-    return 'This action returns a cat';
+  find(@Req() request: Request): string {
+    console.log(request.url)
+    return `This action returns a cat.<br>Request to ${request.url} from ${request.ip}`;
   }
+  
   @Get('all')
   findAll(): string {
     return 'This action returns all cats';
+  }
+  
+  // @Get('google')
+  // @Redirect('https://google.com', 301)
+  // getDocs() {}
+  
+  @Get(':id')
+  findOne(@Param() params): string {
+    console.log(params.id);
+    return `This action returns a #${params.id} cat`;
+  }
+}
+
+@Controller({ host: 'admin.localhost' })
+export class AdminController {
+  @Get()
+  index(): string {
+    return 'Admin page';
   }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController, CatsController } from './app.controller';
+import { AdminController, AppController, CatsController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
   imports: [],
-  controllers: [AppController, CatsController],
+  controllers: [AdminController, AppController, CatsController],
   providers: [AppService],
 })
 export class AppModule {}
